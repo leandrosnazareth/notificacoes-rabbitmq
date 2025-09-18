e51cbf48884883626d47c46928934b1951acf343
leandro.nazareth <leandro.nazareth@mgs.srv.br>
2025-09-18 10:00:42 -0300

    initial

---
+++ src/main/java/com/leandronazareth/notificacoes_rabbitmq/NotificacoesRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.leandronazareth.notificacoes_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacoesRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacoesRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandronazareth/notificacoes_rabbitmq/NotificacoesRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandronazareth.notificacoes_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacoesRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
